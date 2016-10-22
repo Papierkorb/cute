b639e7d1475de054f2e39febc27837263e0b1fb8
Stefan Merettig <stefan-merettig@nuriaproject.org>
2016-10-22 14:11:55 +0200

    Add example code for Cute.spy

---
+++ samples/testing.cr
@@ -0,0 +1,30 @@
+require "../src/cute"
+
+# Demonstrates the signal spy mechanism for testing purposes.
+
+require "spec"
+require "../src/cute/spec"
+
+class Button
+  Cute.signal clicked(x : Int32, y : Int32)
+
+  def click
+    clicked.emit(4, 5)
+  end
+end
+
+describe Button do
+  describe "#click" do
+    it "emits #clicked" do
+      btn = Button.new
+
+      # Build the spy. Just copy the signal definition over.
+      clicked_spy = Cute.spy btn, clicked(x : Int32, y : Int32)
+      btn.click # Somehow trigger a signal emission
+
+      # The signal spy inherits from an array, so verification is easy
+      clicked_spy.size.should eq 1
+      clicked_spy.first.should eq({ 4, 5 })
+    end
+  end
+end
